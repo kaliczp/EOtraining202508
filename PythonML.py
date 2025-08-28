babffd2ddbe503af647d8b15cebe68ae853e5323
Kalicz Péter <kaliczp@gmail.com>
2025-08-28 09:55:51 +0200

    Python init

---
+++ PythonML.py
@@ -0,0 +1,6 @@
+# pip install --user matplotlib
+# pip install --user gdown
+import gdown
+gdown.download(id='1j8rnfE3s8m--uasLK7WFAdwubQVvhiuE', output='Sentinel2_Mosaic_2021.tif', quiet=True)
+
+# rasterio torch
